b070b0545f34bcf59a8f7f1d5e7e4d831116902b
bbarnwell25 <89547736+bbarnwell25@users.noreply.github.com>
2023-02-03 12:35:25 -0600

    edited stuff

--- src/GitLab.java
+++ src/GitLab.java
@@ -1,6 +1,6 @@
 public class GitLab {
     public static void main(String[] args) {
-        GitLabHelper helper = new GitLabHelper("John", "Bethany");
+        GitLabHelper helper = new GitLabHelper(new String[] {"John", "Bethany"});
         helper.PrintWelcomeMessage();
     }
 }
