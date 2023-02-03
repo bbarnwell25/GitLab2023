e1027c0bc4dc7cc0a20ffe4e9a4f0a87e9168dbd
john <muffinpost1@gmail.com>
2023-02-03 12:23:57 -0600

    changed to use a helper class

--- src/GitLab.java
+++ src/GitLab.java
@@ -1,5 +1,6 @@
 public class GitLab {
     public static void main(String[] args) {
-        System.out.println("This is a lesson on git.");
+        GitLabHelper helper = new GitLabHelper("John", "Bethany");
+        helper.PrintWelcomeMessage();
     }
 }
